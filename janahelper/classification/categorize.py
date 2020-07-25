b037c6ede23f141085e08566cb2bde1fca82de36
Aumkar Gadekar <45661699+AumkarG@users.noreply.github.com>
2020-07-25 17:23:40 +0530

    Update categorize.py

--- janahelper/classification/categorize.py
+++ janahelper/classification/categorize.py
@@ -2,25 +2,22 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from nltk.corpus import stopwords
-import re
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 sw = stopwords.words('english')
 
 import pickle
-with open('tokenizer.pickle', 'rb') as handle:
+with open('tokenizer.pickle', 'rb') as handle:                                  ## Load tokenizer
     tokenizer = pickle.load(handle)
 
 import tensorflow as tf 
 
-from nltk.stem import WordNetLemmatizer 
+from nltk.stem import WordNetLemmatizer  
 lemmatizer = WordNetLemmatizer() 
-m = tf.keras.models.load_model('m.h5')
+m = tf.keras.models.load_model('m.h5')                                          ## Load model file
 
-y=['Mobility - Roads, Footpaths and Infrastructure',
+y=['Mobility - Roads, Footpaths and Infrastructure',         
          'Garbage and Unsanitary Practices',
          'Traffic and Road Safety',
          'Water Supply and Services',
@@ -43,14 +40,13 @@ y=['Mobility - Roads, Footpaths and Infrastructure',
          'Trees and Saplings',
          'Fire Safety',]
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder                                   
 labelEncoder = LabelEncoder()
-output = labelEncoder.fit_transform(y)
+output = labelEncoder.fit_transform(y)                                          ## Convert categories to numerical values
 
 def pred(sentence):
     sentence=[sentence]
-    for x in range(len(sentence)):
+    for x in range(len(sentence)):                                              ## remove stopwards and lemmatize
         print(x)
         s=sentence[x].split()
         s1=[]
@@ -60,10 +56,10 @@ def pred(sentence):
         sentence[x]=' '.join(s1)
 
 
-    test = tokenizer.texts_to_sequences(sentence)
+    test = tokenizer.texts_to_sequences(sentence)                               ## tokenize sentence and pad 0s
     test = pad_sequences(test,maxlen=14)
 
-    x=labelEncoder.inverse_transform([np.argmax(m.predict(test))])
+    x=labelEncoder.inverse_transform([np.argmax(m.predict(test))])              ## Predict
     print(x[0])
 
-    return x[0]
+    return x[0]                                                                  ## Return prediction
