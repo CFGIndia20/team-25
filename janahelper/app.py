62700b58253ce1345294380b3f28f8d5b2aecc60
Akshaz Singh <akshazs7@gmail.com>
2020-07-26 00:52:13 +0530

    Removing .env

--- janahelper/app.py
+++ janahelper/app.py
@@ -1,21 +1,21 @@
 from googletrans import Translator
 from classification.categorize import pred
 from flask import Flask, render_template,request, redirect, url_for
-translator = Translator()
 
 app = Flask(__name__)
+translator = Translator()
 
 @app.route('/complaints', methods=['POST','GET'])
 def get_data():
     if request.method == 'GET':
         return render_template('input.html')
     elif request.method == 'POST':
-            text = request.form['complaint']
-            text = translator.translate(str(text), dest='en').text
-            latitude = request.form['cdlat']
-            longitude = request.form['cdlon']
-            categories=pred(text)
-            return {'categories': categories, 'location': {"latitude": latitude, "longitude": longitude}}
+        text = request.form['complaint']
+        text = translator.translate(str(text), dest='en').text
+        latitude = request.form['cdlat']
+        longitude = request.form['cdlon']
+        categories=pred(text)
+        return {'categories': categories, 'location': {"latitude": latitude, "longitude": longitude}}
 
 
 @app.errorhandler(404)  
