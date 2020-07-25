5bce2b22121f4bf2f9649bf9d657e92f4e304f0a
Akshaz Singh <akshazs7@gmail.com>
2020-07-25 16:18:58 +0530

    Api done

--- janahelper/app.py
+++ janahelper/app.py
@@ -2,38 +2,22 @@ from flask import Flask, render_template, request
 import csv, json
 app = Flask(__name__)
 
-# with open('file.csv', 'r') as file:
-#     my_reader = csv.reader(file, delimiter=',')
-#     for row in my_reader:
-#         pass
-
-def category_by_hand(text):
-    pass
-
-def location_by_hand(text):
-    pass
-
 
 def get_categories(text):
-    pass
+    return {"hello": text}
 
-def get_location():
-    pass
+def get_location(text):
+    return {text: "world"}
 
 @app.route('/complaints', methods=['POST','GET'])
 def get_data():
     if request.method == 'GET':
         return render_template('input.html')
     elif request.method == 'POST':
-        for i in request.form.keys():
-            print(i)
-        return render_template("input.html")
-        # text = request.form['complaint']
-        # categories = get_categories(text)
-        # location = get_location(text)
-        # return json.dump({'categories': categories, 'location': location})
-
-
+        text = request.form['complaint']
+        categories = get_categories(text)
+        location = get_location(text)
+        return {'categories': categories, 'location': location}
 
 if __name__ == '__main__':
     app.run(debug=True, port="8000")
