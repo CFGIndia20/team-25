f60f870388c7fc8350cc49c9aaa7a91db4ecee3b
nippun99 <37634751+nippun99@users.noreply.github.com>
2020-07-25 17:43:23 +0530

    conflict junk removed

--- janahelper/app.py
+++ janahelper/app.py
@@ -8,17 +8,6 @@ def get_data():
     if request.method == 'GET':
         return render_template('input.html')
     elif request.method == 'POST':
-<<<<<<< HEAD
-        text = request.form['complaint']
-        lat= request.form['cdlat']
-        lon= request.form['cdlon']
-        print(lat)
-        categories = get_categories(text)
-        location = get_location()
-        return {'categories': text, 'latitude': lat , 'longitude':lon}
-
-
-=======
         try:
             text = request.form['complaint']
             latitude = request.form['cdlat']
@@ -31,7 +20,6 @@ def get_data():
 @app.errorhandler(404)  
 def not_found(e):
     return redirect(url_for('get_data'))
->>>>>>> 996878ccce5b6a35438e82f2a1a220c6f528f848
 
 if __name__ == '__main__':
     app.run(debug=True, )
