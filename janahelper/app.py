422b2d97eb731b270158238c8957facea49c77c0
Akshaz Singh <akshazs7@gmail.com>
2020-07-25 14:49:33 +0530

    Added templates

--- janahelper/app.py
+++ janahelper/app.py
@@ -1,18 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+import csv, json
 app = Flask(__name__)
 
+with open('file.csv', 'r') as file:
+    my_reader = csv.reader(file, delimiter=',')
+    for row in my_reader:
+        print(row)
 
 
+def category_by_hand(text):
+    pass
 
+def location_by_hand(text):
+    pass
+
+
+def get_categories(text):
+    pass
+
+def get_location():
+    pass
 
 @app.route('/complaints', methods=['POST','GET'])
 def get_data():
-    if(request.method == 'POST'):
-        pass
-    else:
-        return render_template('hello.html')
+    if request.method == 'GET':
+        return render_template('input.html')
+    elif request.method == 'POST':
+        text = request.form['complaint']
+        categories = get_categories(text)
+        location = get_location(text)
+        return json.dump({'categories': categories, 'location': location})
 
-   pass
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, )
