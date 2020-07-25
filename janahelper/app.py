bf018fc752f80026c9b5fd3c2038d2bd94cea656
Akshaz Singh <akshazs7@gmail.com>
2020-07-25 17:31:22 +0530

    Added categorization to flask

--- janahelper/app.py
+++ janahelper/app.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template, request
-import csv, json
-app = Flask(__name__)
+import csv, json, os
+from dotenv import load_dotenv
+from classification.categorize import pred
+from flask import Flask, render_template, request, url_for, redirect
 
+load_dotenv()
+print(os.getenv("API_KEY"))
+app = Flask(__name__)
 
 def get_categories(text):
     return {"hello": text}
-
-def get_location(text):
-    return {text: "world"}
 
 @app.route('/complaints', methods=['POST','GET'])
 def get_data():
     if request.method == 'GET':
         return render_template('input.html')
     elif request.method == 'POST':
-        text = request.form['complaint']
-        categories = get_categories(text)
-        location = get_location(text)
-        return {'categories': categories, 'location': location}
+        try:
+            text = request.form['complaint']
+            latitude = request.form['latitude']
+            longitude = request.form['longitude']
+            categories = pred(text)
+            return {'categories': categories, 'location': {"latittude": latitude, "longitude": longitude}}
+        except:
+            return redirect(url_for('get_data'))
+
+@app.errorhandler(404)  
+def not_found(e):
+    return redirect(url_for('get_data'))
 
 if __name__ == '__main__':
     app.run(debug=True, port="8000")
